5d49173421b3f6a0fbafcf37daa3c068dc5c2119
TouchMyToes <averycooper08@gmail.com>
2022-08-01 16:17:56 -0400

    did absolutely nothing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake.java
@@ -18,11 +18,6 @@ public class Intake {
     double UP_POSITION = 1.0;
     double DOWN_POSITION = 0.525;
 
-//    public enum SweeperPos {
-//        UP, DOWN
-//    }
-
-    //private SweeperPos sweeperPos = SweeperPos.UP;
 
     public Intake(){
         intakeMotor = hardwareMap.get(DcMotor.class, "intake");
@@ -38,32 +33,6 @@ public class Intake {
     public void runIntakeBackwards(){
         intakeMotor.setPower(-1.0);
     }
-
-//    controller.get(SQUARE, TOGGLE)
-//    public void update(boolean square_toggle){
-//
-//        switch(sweeperPos){
-//
-//            case UP:
-//
-//                // set the sweeper position on the servo
-//                sweeper.setPosition(UP_POSITION);
-//                if (square_toggle) sweeperPos = SweeperPos.DOWN;
-//
-//                break;
-//
-//
-//            case DOWN:
-//
-//                // set the sweeper position on the servo
-//                sweeper.setPosition(DOWN_POSITION);
-//                if (!square_toggle) sweeperPos = SweeperPos.UP;
-//
-//                break;
-//
-//        }
-//
-//    }
 
 
 }
