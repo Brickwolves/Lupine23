a54ed1f2396b04ec90faad55a58f8a1728dc0130
OliverB11 <92954010+OliverB11@users.noreply.github.com>
2022-08-18 16:16:59 -0400

    Tuned Color sensor for new bucket, edited duck auto - still not done but mostly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedLinearAutoDuck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedLinearAutoDuck.java
@@ -12,6 +12,7 @@ import static org.firstinspires.ftc.teamcode.DashConstants.DuckPos.left;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
 
+import org.firstinspires.ftc.teamcode.Hardware.DuckSpinner;
 import org.firstinspires.ftc.teamcode.Hardware.Robot;
 import org.firstinspires.ftc.teamcode.Utilities.Loggers.Side;
 
@@ -30,8 +31,7 @@ public class RedLinearAutoDuck extends LinearOpMode
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() {
 
         initialize();
 
@@ -43,22 +43,36 @@ public class RedLinearAutoDuck extends LinearOpMode
 
         waitForStart();
 
-        if (opModeIsActive()){
-            if(left){
+        if (opModeIsActive()) {
+            if (left) {
                 robot.scorer.autoLow();
-                robot.drivetrain.strafe(.8,830,0,90, robot.gyro);
-                robot.drivetrain.strafe(.4,280,0,180, robot.gyro);
+                robot.drivetrain.strafe(.8, 830, 0, 90, robot.gyro);
+                robot.drivetrain.strafe(.4, 270, 0, 180, robot.gyro);
 
-            }else{
+            } else {
                 robot.scorer.autoHigh();
-                robot.drivetrain.strafe(.8,800,0,90, robot.gyro);
-                robot.drivetrain.strafe(.4,300,0,180, robot.gyro);
+                robot.drivetrain.strafe(.8, 800, 0, 90, robot.gyro);
+                robot.drivetrain.strafe(.4, 270, 0, 180, robot.gyro);
             }
             robot.sleep(.5);
             robot.scorer.autoDeposit();
-            robot.drivetrain.strafe(.8,400,315,315,robot.gyro);
-            robot.drivetrain.strafe(.5,1000,315,270,robot.gyro);
+            robot.drivetrain.strafe(.6, 2000, 0, 270, robot.gyro, 6, 0.5, 0.1);
+            robot.drivetrain.strafe(.15, 300, 0, 0, robot.gyro);
+            robot.duck.spin();
+            robot.sleep(3);
+            robot.duck.stop();
+            if (left) {
+                robot.drivetrain.strafe(.5, 400, 0, 180, robot.gyro);
+                while (robot.drivetrain.blColor.updateRed() < 120) {
+                    robot.drivetrain.foreverDriveStraight(-.2, 0, robot.gyro);
+                }
+                while (robot.drivetrain.flColor.updateRed() < 120) {
+                    robot.drivetrain.foreverDriveStraight(.2, 0, robot.gyro);
+                }
+                robot.drivetrain.strafe(.2, 50, 0, 180, robot.gyro);
+
+
+            }
         }
     }
 }
-
